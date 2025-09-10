7176e4452070b70062b524ac31688422f71e9654
kenneth <kenlavoie@protonmail.com>
2025-09-10 15:23:43 -0400

    added fields and methods

--- src/main/java/model/Library.java
+++ src/main/java/model/Library.java
@@ -1,4 +1,22 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> books;
+    private ArrayList<Author> authors;
+
+    public Library() {
+        books = new ArrayList<>();
+        authors = new ArrayList<>();
+    }
+
+    public Library(ArrayList<Book> listOfBooks, ArrayList<Author> listOfAuthors) {
+        this.books = new ArrayList<>(listOfBooks);
+        this.authors = new ArrayList<>(listOfAuthors);
+    }
+
+    public ArrayList<Book> getBooks() { return books; }
+
+    public ArrayList<Author> getAuthors() { return authors; }
 }
