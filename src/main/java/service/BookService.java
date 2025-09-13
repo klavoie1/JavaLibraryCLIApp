0fef45caa429beeb65871b59754ab2b9567b670c
kenneth <kenlavoie@protonmail.com>
2025-09-13 00:38:56 -0400

    adde summary method to Book

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -1,4 +1,21 @@
 package service;
 
+import model.Book;
+
+/**
+ * @author kenneth
+ */
 public class BookService {
+
+    public static String bookSummary(Book book) {
+        return "\n------------Book Summary------------\n" +
+                "Title: " + book.getTitle() + ",\n" +
+                "Author: " + book.getAuthor() + ",\n" +
+                "# of Pages: " + book.getNumOfPages() + ",\n" +
+                "Available: " + book.isAvailable();
+    }
+
+    public static void addBook(Book book) {
+
+    }
 }
