1a1f5260ad092b563026f99f06a43ffb7cc07882
kenneth <kenlavoie@protonmail.com>
2025-09-15 09:52:58 -0400

    added list all authors

---
+++ src/main/java/service/LibraryService.java
@@ -0,0 +1,39 @@
+package service;
+
+import model.Author;
+import model.Book;
+import model.Library;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author kenneth
+ */
+public class LibraryService {
+    Scanner scnr = new Scanner(System.in);
+
+    public void addBook(Library library, Book book) {
+        Library.getBooks().add(book);
+    }
+
+    public void removeBook(Library library, Book book) {
+        Library.getBooks().remove(book);
+    }
+
+    public void addAuthor(Library library, Author author) {
+        Library.getAuthors().add(author);
+    }
+
+    public void removeAuthor(Library library, Author author) {
+        Library.getAuthors().remove(author);
+    }
+
+    public ArrayList<Book> listAllBooks(Library library) {
+        return new ArrayList<>(Library.getBooks()); // defensive copy
+    }
+
+    public ArrayList<Author> listAllAuthors(Library library) {
+        return new ArrayList<>(Library.getAuthors());
+    }
+}
